17bcc6b5fee54929df6efc3883c97385f6718797
Mårten Åsberg <marten.asberg@outlook.com>
2020-11-01 10:05:36 +0100

    Piece enum
    And helpers

---
+++ Model/Piece.cs
@@ -0,0 +1,14 @@
+﻿namespace Polytet.Model
+{
+	public enum Piece : byte
+	{
+		Empty = 0b_0000,
+		I = 0b_0001,
+		J = 0b_0010,
+		L = 0b_0011,
+		O = 0b_0100,
+		S = 0b_0101,
+		T = 0b_0110,
+		Z = 0b_0111,
+	}
+}
---
+++ Model/PieceHelper.cs
@@ -0,0 +1,32 @@
+﻿namespace Polytet.Model
+{
+	public static class PieceHelper
+	{
+		public static bool TryGetPieceFromTop(this byte b, out Piece piece)
+		{
+			return TryGetPieceFromBottom((byte)(b >> 4), out piece);
+		}
+		public static bool TryGetPieceFromBottom(this byte b, out Piece piece)
+		{
+			if (typeof(Piece).IsEnumDefined(b))
+			{
+				piece = (Piece)b;
+				return true;
+			}
+			else
+			{
+				piece = Piece.Empty;
+				return false;
+			}
+		}
+
+		public static byte SetPieceToTop(this byte b, Piece piece)
+		{
+			return (byte)(b & 0b_0000_1111 | (byte)((byte)piece << 4));
+		}
+		public static byte SetPieceToBottom(this byte b, Piece piece)
+		{
+			return (byte)(b & 0b_1111_0000 | (byte)piece);
+		}
+	}
+}
