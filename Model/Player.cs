942800b022a5980d6c8baa764222d0a2f3202404
Mårten Åsberg <marten.asberg@outlook.com>
2020-11-02 22:31:23 +0100

    A class that "plays" the game
    Moves forward in ticks and supplies pieces

---
+++ Model/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Timers;
+
+namespace Polytet.Model
+{
+	public class Player : IDisposable
+	{
+		private readonly Game game;
+		private readonly Random random;
+		private readonly Timer timer;
+
+		public Player(Game game, int millisecondInterval, Random? random = null)
+		{
+			this.game = game ?? throw new ArgumentNullException(nameof(game));
+			this.random = random ?? new Random();
+
+			timer = new Timer(millisecondInterval);
+			timer.Elapsed += Timer_Elapsed;
+		}
+
+		public void Start()
+		{
+			if (!timer.Enabled)
+			{
+				EnsureHasNextPiece();
+
+				timer.Start();
+			}
+		}
+
+		private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			game.Tick();
+
+			EnsureHasNextPiece();
+		}
+
+		private void EnsureHasNextPiece()
+		{
+			if (!game.NextPiece.HasValue)
+			{
+				game.AddCommingPiece((Piece)random.Next((int)Piece.I, (int)Piece.Z + 1));
+			}
+		}
+
+		public void Stop()
+		{
+			if (timer.Enabled)
+			{
+				timer.Stop();
+			}
+		}
+
+		public void Dispose()
+		{
+			Stop();
+			timer.Dispose();
+		}
+	}
+}
